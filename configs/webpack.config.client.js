a054c1e273351e0e6abec240bb9aa61ed77e65cb
honghaibin <15867179361@163.com>
2018-01-26 14:49:44 +0800

    针对打包做的webpack配置优化

--- configs/webpack.config.client.js
+++ configs/webpack.config.client.js
@@ -51,6 +51,45 @@ if (isDev) {
 	config.plugins.push(new webpack.HotModuleReplacementPlugin())
 } else {
 	//开发之外需要对打包进行的优化
+  // 把公共的代码单独打包，这样浏览器就不会每次都加载这些包了
+  config.entry = {
+    app: path.join(__dirname, '../client/index-client.js'),
+    vendor: [
+      'react',
+      'react-dom',
+      'react-router-dom',
+      'redux',
+      'react-redux',
+      'axios',
+      'echarts'
+    ]
+  }
+  // 设置hash值为模块的hash值，而不是所有打包文件的hash值
+  config.output.filename = '[name].[chunkhash].js';
+  // 1.压缩代码 2.指定vendor里面打包的代码不再打包到app....js里面
+  // 3.webpack每次都会自动生成一些东西从而导致vendor每次都不一样，所以需要配置
+  config.plugins.push(
+    new webpack.optimize.UglifyJsPlugin(),
+    new webpack.optimize.CommonsChunkPlugin({
+      name: 'vendor'
+    }),
+    new webpack.optimize.CommonsChunkPlugin({
+      name: 'manifest',
+      minChunks: Infinity
+    }),
+    new webpack.NamedModulesPlugin(),
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: JSON.stringify('production')
+      }
+    }),
+    new webpack.NamedChunksPlugin((chunk) => {
+      if (chunk.name) {
+        return chunk.name;
+      }
+      return chunk.mapModules(m => path.relative(m.context, m.request)).join('_')
+    })
+  )
 }
 
 module.exports = config
